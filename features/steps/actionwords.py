eb8c20378aa1330e42ce6aa222931f02ed16cefd
Vincent Pretre <vincent.pretre@gmail.com>
2018-04-04 22:12:46 +0200

    Sample on how to use context with actionwords

--- features/steps/actionwords.py
+++ features/steps/actionwords.py
@@ -7,80 +7,80 @@ class Actionwords:
         self.sut = CoffeeMachine()
         self.handled = []
 
-    def i_start_the_coffee_machine_using_language_lang(self, lang = "en"):
+    def i_start_the_coffee_machine_using_language_lang(self, context, lang = "en"):
         self.sut.start(lang)
 
-    def i_shutdown_the_coffee_machine(self):
+    def i_shutdown_the_coffee_machine(self, context):
         self.sut.stop()
 
-    def message_message_should_be_displayed(self, message):
+    def message_message_should_be_displayed(self, context, message = ""):
         assert (self.sut.message == message) is True
 
-    def coffee_should_be_served(self):
-        assert self.sut.coffee_served is True
+    def coffee_should_be_served(self, context):
+        assert (self.sut.message == message) is True
 
-    def coffee_should_not_be_served(self):
+    def coffee_should_not_be_served(self, context):
         assert self.sut.coffee_served is False
 
-    def i_take_a_coffee(self):
+    def i_take_a_coffee(self, context):
         self.sut.take_coffee()
 
-    def i_empty_the_coffee_grounds(self):
+    def i_empty_the_coffee_grounds(self, context):
         self.sut.empty_grounds()
 
-    def i_fill_the_beans_tank(self):
-        self.sut.fill_beans()
+    def i_fill_the_beans_tank(self, context):
+        self.sut.empty_grounds()
 
-    def i_fill_the_water_tank(self):
+    def i_fill_the_water_tank(self, context):
         self.sut.fill_tank()
 
-    def i_take_coffee_number_coffees(self, coffee_number = 10):
+    def i_take_coffee_number_coffees(self, context, coffee_number = 10):
         coffee_number = int(coffee_number)
 
         while (coffee_number > 0):
-            self.i_take_a_coffee()
+            self.i_take_a_coffee(context)
             coffee_number = coffee_number - 1
 
             if 'water' in self.handled:
-                self.i_fill_the_water_tank()
+                self.i_fill_the_water_tank(context)
 
             if 'beans' in self.handled:
-                self.i_fill_the_beans_tank()
+                self.i_fill_the_beans_tank(context)
 
             if 'grounds' in self.handled:
-                self.i_empty_the_coffee_grounds()
+                self.i_empty_the_coffee_grounds(context)
 
-    def the_coffee_machine_is_started(self):
-        self.i_start_the_coffee_machine_using_language_lang()
+    def the_coffee_machine_is_started(self, context):
+        self.i_start_the_coffee_machine_using_language_lang(context)
 
-    def i_handle_water_tank(self):
+    def i_handle_everything_except_the_water_tank(self, context):
+        self.i_handle_coffee_grounds(context)
+        self.i_handle_beans(context)
+
+    def i_handle_water_tank(self, context):
         self.handled.append('water')
 
-    def i_handle_beans(self):
+    def i_handle_beans(self, context):
         self.handled.append('beans')
 
-    def i_handle_coffee_grounds(self):
+    def i_handle_coffee_grounds(self, context):
         self.handled.append('grounds')
 
-    def i_handle_everything_except_the_water_tank(self):
-        self.i_handle_coffee_grounds()
-        self.i_handle_beans()
+    def i_handle_everything_except_the_beans(self, context):
+        self.i_handle_water_tank(context)
+        self.i_handle_coffee_grounds(context)
 
-    def i_handle_everything_except_the_beans(self):
-        self.i_handle_water_tank()
-        self.i_handle_coffee_grounds()
+    def i_handle_everything_except_the_grounds(self, context):
+        self.i_handle_water_tank(context)
+        self.i_handle_beans(context)
 
-    def i_handle_everything_except_the_grounds(self):
-        self.i_handle_water_tank()
-        self.i_handle_beans()
+    def displayed_message_is(self, context, free_text = ""):
+        self.message_message_should_be_displayed(context, message = __free_text)
 
-    def displayed_message_is(self, free_text = ""):
-        self.message_message_should_be_displayed(message = free_text)
-
-    def i_switch_to_settings_mode(self):
+    def i_switch_to_settings_mode(self, context):
         self.sut.show_settings()
 
-    def settings_should_be(self, datatable = "||"):
+    def settings_should_be(self, context, datatable = "||"):
         # Apparently, no way to get the raw table and assert_equals does not work that much ...
         expected = [datatable.rows[0].headings]
         for row in datatable.rows:
--- features/steps/steps.py
+++ features/steps/steps.py
@@ -11,63 +11,62 @@ use_step_matcher('re')
 
 @when(r'I start the coffee machine using language "(.*)"')
 def impl(context, lang = "en"):
-    context.actionwords.i_start_the_coffee_machine_using_language_lang(lang)
+    context.actionwords.i_start_the_coffee_machine_using_language_lang(context, lang)
 
 
 @when(r'I shutdown the coffee machine')
 def impl(context):
-    context.actionwords.i_shutdown_the_coffee_machine()
+    context.actionwords.i_shutdown_the_coffee_machine(context)
 
 
 @then(r'message "(.*)" should be displayed')
 def impl(context, message = ""):
-    context.actionwords.message_message_should_be_displayed(message)
+    context.actionwords.message_message_should_be_displayed(context, message)
 
 
 @then(r'coffee should be served')
 def impl(context):
-    context.actionwords.coffee_should_be_served()
+    context.actionwords.coffee_should_be_served(context)
 
 
 @then(r'coffee should not be served')
 def impl(context):
-    context.actionwords.coffee_should_not_be_served()
+    context.actionwords.coffee_should_not_be_served(context)
 
 
 @when(r'I take a coffee')
 def impl(context):
-    context.actionwords.i_take_a_coffee()
+    context.actionwords.i_take_a_coffee(context)
 
 
 @when(r'I empty the coffee grounds')
 def impl(context):
-    context.actionwords.i_empty_the_coffee_grounds()
+    context.actionwords.i_empty_the_coffee_grounds(context)
 
 
 @when(r'I fill the beans tank')
 def impl(context):
-    context.actionwords.i_fill_the_beans_tank()
+    context.actionwords.i_fill_the_beans_tank(context)
 
 
 @when(r'I fill the water tank')
 def impl(context):
-    context.actionwords.i_fill_the_water_tank()
+    context.actionwords.i_fill_the_water_tank(context)
 
 
 @when(r'I take "(.*)" coffees')
-@given(r'I take "(.*)" coffees')
 def impl(context, coffee_number = 10):
-    context.actionwords.i_take_coffee_number_coffees(coffee_number)
+    context.actionwords.i_take_coffee_number_coffees(context, coffee_number)
 
 
 @given(r'the coffee machine is started')
 def impl(context):
-    context.actionwords.the_coffee_machine_is_started()
+    context.actionwords.the_coffee_machine_is_started(context)
 
 
 @given(r'I handle everything except the water tank')
 def impl(context):
-    context.actionwords.i_handle_everything_except_the_water_tank()
+    context.actionwords.i_handle_everything_except_the_water_tank(context)
 
 
 
@@ -78,24 +77,24 @@ def impl(context):
 
 @given(r'I handle everything except the beans')
 def impl(context):
-    context.actionwords.i_handle_everything_except_the_beans()
+    context.actionwords.i_handle_everything_except_the_beans(context)
 
 
 @given(r'I handle everything except the grounds')
 def impl(context):
-    context.actionwords.i_handle_everything_except_the_grounds()
+    context.actionwords.i_handle_everything_except_the_grounds(context)
 
 
 @then(r'displayed message is')
 def impl(context, free_text = ""):
-    context.actionwords.displayed_message_is(context.text)
+    context.actionwords.displayed_message_is(context, context.text)
 
 
 @when(r'I switch to settings mode')
 def impl(context):
-    context.actionwords.i_switch_to_settings_mode()
+    context.actionwords.i_switch_to_settings_mode(context)
 
 
 @then(r'settings should be')
 def impl(context, datatable = "||"):
-    context.actionwords.settings_should_be(context.table)
+    context.actionwords.settings_should_be(context, context.table)
